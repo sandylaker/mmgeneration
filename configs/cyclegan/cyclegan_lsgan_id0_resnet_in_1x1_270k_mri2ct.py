3ff80888a0b284a8531019f0ecb2fce03313e6f5
sandylaker <yawei.li@tum.de>
2021-07-22 09:55:08 +0200

    add mri2ct config

---
+++ configs/cyclegan/cyclegan_lsgan_id0_resnet_in_1x1_270k_mri2ct.py
@@ -0,0 +1,34 @@
+_base_ = [
+    '../_base_/models/cyclegan_lsgan_resnet.py',
+    '../_base_/datasets/unpaired_imgs_256x256.py',
+    '../_base_/default_runtime.py'
+]
+model = dict(id_loss=dict(type='L1Loss', loss_weight=0, reduction='mean'))
+dataroot = './data/kaggle_mri2ct/images'
+data = dict(
+    samples_per_gpu=16,
+    val_samples_per_gpu=16,
+    train=dict(dataroot=dataroot),
+    val=dict(dataroot=dataroot),
+    test=dict(dataroot=dataroot))
+
+optimizer = dict(
+    generators=dict(type='Adam', lr=0.0002, betas=(0.5, 0.999)),
+    discriminators=dict(type='Adam', lr=0.0002, betas=(0.5, 0.999)))
+lr_config = None
+checkpoint_config = dict(interval=40000, save_optimizer=False, by_epoch=False)
+custom_hooks = [
+    dict(
+        type='MMGenVisualizationHook',
+        output_dir='training_samples',
+        res_name_list=['fake_b'],
+        interval=40000)
+]
+
+runner = None
+use_ddp_wrapper = True
+total_iters = 270000
+workflow = [('train', 1)]
+metrics = dict(
+    FID=dict(type='FID', num_images=140, image_shape=(3, 256, 256)),
+    IS=dict(type='IS', num_images=140, image_shape=(3, 256, 256)))
