ffa5c45187d58f6196b3bd10a82b7c39276295e5
sandylaker <yawei.li@tum.de>
2021-07-29 11:45:00 +0200

    update mri2ct config

--- configs/cyclegan/cyclegan_lsgan_id0_resnet_in_1x1_270k_mri2ct.py
+++ configs/cyclegan/cyclegan_lsgan_id0_resnet_in_1x1_270k_mri2ct.py
@@ -6,23 +6,28 @@ _base_ = [
 model = dict(id_loss=dict(type='L1Loss', loss_weight=0, reduction='mean'))
 dataroot = './data/kaggle_mri2ct/images'
 data = dict(
-    samples_per_gpu=16,
+    samples_per_gpu=8,
     val_samples_per_gpu=16,
     train=dict(dataroot=dataroot),
     val=dict(dataroot=dataroot),
     test=dict(dataroot=dataroot))
 
 optimizer = dict(
-    generators=dict(type='Adam', lr=0.0002, betas=(0.5, 0.999)),
-    discriminators=dict(type='Adam', lr=0.0002, betas=(0.5, 0.999)))
-lr_config = None
-checkpoint_config = dict(interval=40000, save_optimizer=False, by_epoch=False)
+    generators=dict(type='Adam', lr=0.001, betas=(0.5, 0.999)),
+    discriminators=dict(type='Adam', lr=0.001, betas=(0.5, 0.999)))
+lr_config = dict(
+    policy='CosineAnnealing',
+    warmup='linear',
+    warmup_iters=2000,
+    warmup_ratio=1.0 / 10,
+    min_lr_ratio=1e-2)
+checkpoint_config = dict(interval=50000, save_optimizer=False, by_epoch=False)
 custom_hooks = [
     dict(
         type='MMGenVisualizationHook',
         output_dir='training_samples',
         res_name_list=['fake_b'],
-        interval=40000)
+        interval=50000)
 ]
 
 runner = None
